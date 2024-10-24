c0e1cc10d3cd470e3cab7e12e93c79461b72414a
Alylson Monteiro <alylsonmonteiro@lammp-it.com.br>
2024-10-24 14:39:07 -0300

    Validação da lógica de notificação

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -1,40 +1,95 @@
 class ScrapingsController < ApplicationController
-  require 'puppeteer-ruby'
+  #require 'puppeteer-ruby'
+
+  before_action :set_scrapings_params, only: %i[show update destroy]
 
   def index
-    url = params[:url]
-    task_id = params[:task_id]
+    @scrapings = Scraping.all
 
-    Puppeteer.launch(headless: true, args: ['--no-sandbox']) do |browser|
-      page = browser.new_page
-      page.goto(url)
-
-      price = '299.00'
-      brand = 'BMW'
-      model = 'X1'
-
-      if price.nil?
-        render json: { error: 'Could not retrieve data. Please check the selectors or the page structure.' }, status: :bad_request
-      else
-        scraping_record = Scraping.new(price: price.strip, brand: brand.strip, model: model.strip)
-
-        if scraping_record.save
-          ActiveSupport::Notifications.instrument('scraping.created', task_id: task_id, scraping: scraping_record)
-
-          render json: {
-            price: scraping_record.price,
-            brand: scraping_record.brand,
-            model: scraping_record.model
-          }
-        else
-          render json: { error: 'Could not save data to the database.' }, status: :internal_server_error
-        end
-      end
-    end
-
-  rescue Puppeteer::TimeoutError => e
-    render json: { error: "Could not retrieve data: #{e.message}" }, status: :bad_request
-  rescue StandardError => e
-    render json: { error: e.message }, status: :internal_server_error
+    render json: { scrapings: @scrapings }, status: :ok
   end
+
+  def create
+    # url = params[:url]
+    # task_id = params[:task_id]
+
+    scraping_record = Scraping.new(scraping_params)
+    
+    scraping_record.price = "299.00"
+    scraping_record.brand = "BMW"
+    scraping_record.model = "X1"
+
+    if scraping_record.save
+
+      ActiveSupport::Notifications.instrument('scraping.created', scrapings: scraping_record)
+
+      render json: { message: 'Scraping created successfully', scraping: scraping_record }, status: :created
+    else
+      render json: { error: 'Could not save data to the database.' }, status: :internal_server_error
+    end
+    # Puppeteer.launch(headless: true, args: ['--no-sandbox']) do |browser|
+    #   page = browser.new_page
+    #   page.goto(url)
+
+    #   page.wait_for_function("() => document.querySelector('.VehicleDetailsFipe__price.VehicleDetailsFipe__price--announced') !== null")
+    #   price = page.eval_on_selector('.VehicleDetailsFipe__price__value', 'el => el.textContent')
+    #   brand = page.eval_on_selector('#VehicleBasicInformationTitle', 'el => el.textContent')
+    #   model = page.eval_on_selector('.VehicleDetails__header__title strong', 'el => el.textContent')
+
+    #   if price.nil? || brand.nil? || model.nil?
+    #     render json: { error: 'Could not retrieve data. Please check the selectors or the page structure.' }, status: :bad_request
+    #   else
+    #     scraping_record = Scraping.new(price: price.strip, brand: brand.strip, model: model.strip)
+
+    #     if scraping_record.save
+
+    #       Rails.logger.info "Attempting to notify scraping.created with task_id: #{task_id} and scraping_record: #{scraping_record.inspect}"
+    #       ActiveSupport::Notifications.instrument('scraping.created', scraping: scraping_record)
+
+    #       render json: {
+    #         price: scraping_record.price,
+    #         brand: scraping_record.brand,
+    #         model: scraping_record.model
+    #       }
+    #     else
+    #       render json: { error: 'Could not save data to the database.' }, status: :internal_server_error
+    #     end
+    #   end
+    # end
+
+  # rescue Puppeteer::TimeoutError => e
+  #   render json: { error: "Could not retrieve data: #{e.message}" }, status: :bad_request
+  # rescue StandardError => e
+  #   render json: { error: e.message }, status: :internal_server_error
+  end
+
+  def show
+    render json: @scraping, status: :ok
+  end
+
+  def update
+    if @scraping.update(scrapings_params)
+      render json: { message: 'Scraping Updated Successfully', scraping: @scraping }, status: :ok
+    else
+      render json: { errors: @scraping.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @scraping.destroy
+      render json: { message: 'Scraping Deleted Successfully' }, status: :ok
+    else
+      render json: { errors: @scraping.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_scraping_params
+    @scraping = Scraping.find(params[:id])
+  end
+
+  def scraping_params
+    params.permit(:brand, :model, :price)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  post 'scrapings', to: 'scrapings#index'
+  #post 'scrapings', to: 'scrapings#index'
+  resources :scrapings, only: %i[index create show update destroy]
+  root "scrapings#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
